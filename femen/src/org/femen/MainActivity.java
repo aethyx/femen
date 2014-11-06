47acc42dae72cad0920da6353273f8d1a0e84f35
c1ph4 <sascha.schroeder@aethyx.com>
2014-11-06 20:08:24 +0100

    FEMEN Android app
    ---
    
    This is an Android project I started after contacting Inna if they would
    like to have FEMEN on Androids.
    
    She said yes, but also that she had something other in mind.
    
    Unfortunately I never heard of any other plans, so I'm releasing this
    into the wild as I don't have very much time to work on it any further.
    
    ---
    
    Basically it's a compilation of different ressources you find on the
    web:
    
    * FEMEN.org
    * a Facebook account (which is shut down often and changes constantly)
    * their Twitter
    * their online shop
    * their PayPal account (which never worked...)
    
    ---
    
    It's working as an app though, maybe someone wants to develop this any
    further or compilate it for themselves.
    
    May the fourth be with you!
    
    ---
    
    Sincerely
    
    Signed-off-by: c1ph4 <sascha.schroeder@aethyx.com>

---
+++ femen/src/org/femen/MainActivity.java
@@ -0,0 +1,169 @@
+//Autor: Sascha Schroeder
+//EMail: sascha.schroeder@aethyx.com
+//Website: http://aethyx.com
+//Datum: ab Ende Juli 2014
+// --- from Sascha to Inna ---
+package org.femen;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.PluginState;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import org.femen.R;
+
+public class MainActivity extends Activity {
+
+	private WebView browser;
+	//ProgressDialog progress;
+	ProgressBar loadingProgressBar;
+	
+	@Override
+    protected void onSaveInstanceState(Bundle outState) {
+        browser.saveState(outState);
+    }
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    //getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+	    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	    setContentView(R.layout.activity_main);
+	    browser = (WebView)findViewById(R.id.webView1);
+	    browser.getSettings().setLoadsImagesAutomatically(true);
+	    browser.getSettings().setLoadWithOverviewMode(true);
+	    browser.getSettings().setUseWideViewPort(true);		    
+	    browser.getSettings().setJavaScriptEnabled(true);
+	    browser.getSettings().setPluginState(PluginState.ON);
+	    browser.getSettings().setBuiltInZoomControls(true);
+	    browser.setVerticalScrollBarEnabled(false);
+	    browser.getSettings().setAllowFileAccess(true);
+	    browser.getSettings().setDatabaseEnabled(true);
+	    browser.getSettings().setDomStorageEnabled(true);
+	    browser.getSettings().setAppCacheEnabled(true);
+	    browser.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+	    
+	    // Enable downloads of files within webView
+        browser.setDownloadListener(new DownloadListener() {
+            public void onDownloadStart(String url, String userAgent,
+                    String contentDisposition, String mimetype,
+                    long contentLength) {
+              Intent i = new Intent(Intent.ACTION_VIEW);
+              i.setData(Uri.parse(url));
+              startActivity(i);
+            }
+        });
+	    
+		// Attach the ProgressBar layout
+        loadingProgressBar=(ProgressBar)findViewById(R.id.progressBar1);
+        browser.setWebChromeClient(new WebChromeClient() {
+
+            // this will be called on page loading progress
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+                super.onProgressChanged(view, newProgress);
+
+                loadingProgressBar.setProgress(newProgress);
+
+                // hide the progress bar if the loading is complete
+                if (newProgress == 100) {
+                loadingProgressBar.setVisibility(View.GONE);
+                } else{
+                loadingProgressBar.setVisibility(View.VISIBLE);
+                }
+            }
+            
+        });
+        
+        browser.setWebViewClient(new WebViewClient(){
+        	// load url
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        if (savedInstanceState != null)
+        {
+        	browser.restoreState(savedInstanceState);
+        }
+        else
+        {
+        	browser.loadUrl("http://femen.org");
+        }
+    }
+	
+	@Override
+	public void onBackPressed (){
+        if (browser.isFocused() && browser.canGoBack()) {
+        	browser.goBack();
+        }else {
+                MainActivity.this.finish();
+        }
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {         
+        switch (item.getItemId())
+        {
+        case R.id.shop:
+            browser.loadUrl("http://femenshop.miiduu.com/");
+            return true;
+        
+        case R.id.home:
+            browser.loadUrl("http://femen.org");
+            return true;            
+            
+        case R.id.flattr:
+            browser.loadUrl("https://www.paypal.com/fr/cgi-bin/webscr?cmd=_flow&SESSION=k7mVl-JfNkV5A5Nvke5YXJ41F3pR1_PsBCECTl3zl2WxtnXMFonmMK7jVTe&dispatch=5885d80a13c0db1f8e263663d3faee8d69a70501aadbc2ff6a1e7e8cc0df6b0b");
+            return true;
+            
+        case R.id.tweet:
+            browser.loadUrl("https://twitter.com/FEMEN_Movement");
+            return true;
+            
+        case R.id.liken:
+            browser.loadUrl("https://www.facebook.com/FEMEN.fra?ref=hl");
+            return true;
+ 
+        case R.id.refresh:
+        	browser.reload();
+            return true;
+ 
+        case R.id.about:
+        	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        	builder.setTitle("FEMEN/ФЕМЕН");
+        	builder.setIcon(R.drawable.ic_launcher);
+        	builder.setMessage("Thank you for supporting FEMEN!" + "\n" + "Version 1.0.0 [BETA]" + "\n" + "Email: femen.ua@gmail.com" + "\n" + "\u00A9 " + "femen.org, 2014");
+        	builder.setPositiveButton("OK", null);
+        	AlertDialog dialog = builder.show();
+
+        	TextView messageView = (TextView)dialog.findViewById(android.R.id.message);
+        	messageView.setGravity(Gravity.LEFT);
+            return true;
+            
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }    
+}
